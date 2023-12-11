269f9d21fdca577b29055931f892ed051a505b9e
Dominic Gernert <gernd1@bfh.ch>
2023-12-11 11:29:49 +0100

    added a regex extension so day01 part 2 can be solved within kotlin

--- AoC2023/src/main/kotlin/Extensions.kt
+++ AoC2023/src/main/kotlin/Extensions.kt
@@ -10,3 +10,11 @@ fun LongRange.intersects(other: LongRange): Boolean {
 fun LongRange.intersection(other: LongRange): LongRange {
     return LongRange(maxOf(first, other.first), minOf(last, other.last))
 }
+
+// https://discuss.kotlinlang.org/t/feature-request-regex-findall-with-overlap/27729
+fun Regex.findAllWithOverlap(input: CharSequence, startIndex: Int = 0): Sequence<MatchResult> {
+    if (startIndex < 0 || startIndex > input.length) {
+        throw IndexOutOfBoundsException("Start index out of bounds: $startIndex, input length: ${input.length}")
+    }
+    return generateSequence({ find(input, startIndex) }, { find(input, it.range.first + 1) })
+}
