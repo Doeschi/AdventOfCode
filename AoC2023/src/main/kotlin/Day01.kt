7dd7838cab63f4d2b38570190e58830976703b90
Dominic Gernert <gernd1@bfh.ch>
2023-12-11 11:30:07 +0100

    added day01 part 2 in kotlin and remove python version

--- AoC2023/src/main/kotlin/Day01.kt
+++ AoC2023/src/main/kotlin/Day01.kt
@@ -32,9 +32,32 @@ class Day01(input: List<String>) {
         (firstDig * 10) + lastDig
     }
 
-    fun solvePart2(): Int {
-        // implemented in python because kotlin regex doesn't
-        // work the same as python regex
-        return 0
+    fun solvePart2(): Int = calibrationDoc.sumOf { line ->
+        val pattern = Regex("""one|two|three|four|five|six|seven|eight|nine|\d""")
+        val r = pattern.findAllWithOverlap(line).toList()
+
+        val firstDig = getValue(r.first().value)
+        val lastDig = getValue(r.last().value)
+
+        (firstDig * 10) + lastDig
+    }
+
+    fun getValue(input: String): Int {
+        try {
+            return input.toInt()
+        } catch (e: NumberFormatException) {
+            return when (input) {
+                "one" -> 1
+                "two" -> 2
+                "three" -> 3
+                "four" -> 4
+                "five" -> 5
+                "six" -> 6
+                "seven" -> 7
+                "eight" -> 8
+                "nine" -> 9
+                else -> throw RuntimeException("CANT PARSE NUMBER: $input")
+            }
+        }
     }
 }
