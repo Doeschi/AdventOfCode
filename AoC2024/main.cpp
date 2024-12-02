c6b8f2188bc957cfde5eb35e5386b2216aa61d70
Dominic Gernert <dgernert@gmail.com>
2024-12-02 21:19:37 +0100

    added single day runner

--- AoC2024/main.cpp
+++ AoC2024/main.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 #include <memory>
 #include <vector>
-#include <utility>
 #include <format>
-
 
 #include "src/BaseDay/BaseDay.h"
 #include "src/Day01/Day01.h"
 #include "src/Day02/Day02.h"
 
-
 void runAllDays() {
     std::vector<std::unique_ptr<BaseDay>> allDays;
+
     allDays.push_back(std::make_unique<Day01>());
+    allDays.push_back(std::make_unique<Day02>());
 
     for (auto i{0}; i < allDays.size(); ++i) {
         std::cout << "---------- DAY " << std::format("{:2}", i + 1) << " ----------" << std::endl;
@@ -20,13 +19,19 @@ void runAllDays() {
         auto& day = allDays[i];
         day->solvePartOne();
         day->solvePartTwo();
+
+        // new line to split days
+        std::cout << std::endl;
     }
 }
 
-int main() {
-    //runAllDays();
-    Day02 day02{};
+void runDay(std::unique_ptr<BaseDay> day) {
+    std::cout << "Running " << typeid(*day).name() << std::endl;
+    day->solvePartOne();
+    day->solvePartTwo();
+}
 
-    day02.solvePartOne();
-    day02.solvePartTwo();
+int main() {
+    runDay(std::make_unique<Day02>());
+    // runAllDays();
 }
